0d239fa92f195a43c9053be28087990eaf123756
jjwhyte1 <75148771+jjwhyte1@users.noreply.github.com>
2020-12-04 16:07:51 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Magic_8ball
+namespace _8ball_2
 {
     class Program
     {
@@ -12,44 +12,17 @@ namespace Magic_8ball
         {
             Console.WriteLine("You have 3 Questions");
             int question = 3;
-            while (question > 0) {
+            string[] responses = { "Without a Doubt.", "As I see it, yes.", "Concentrate and ask again.", "Don't count on it.", "Outlook not so good" };
+            while (question > 0)
+            {
                 Random n = new Random();
-                int r = n.Next(5);
+                int r = n.Next(0, 4);
                 Console.WriteLine("ask me a question and you shall recieve an answer");
                 Console.ReadLine();
-                Console.WriteLine(Getans(r));
+                Console.WriteLine(responses[r]);
                 question--;
-                Console.WriteLine("You have " + question +" questions left");
-            } 
-             Console.WriteLine("You cant ask any more questions");
-            
-        }
-             static string Getans(int ansNum)
-            {
-                string response;
-                switch (ansNum)
-                {
-                    case 0:
-                        response = "Without a Doubt.";
-                        break;
-                    case 1:
-                        response = "As I see it, yes.";
-                        break;
-                    case 2:
-                        response = "Concentrate and ask again.";
-                        break;
-                    case 3:
-                        response = "Don't count on it.";
-                        break;
-                    case 4:
-                        response = "Outlook not so good";
-                        break;
-                    default:
-                        response = "ask again";
-                        break;
-                }
-                return response;
+                Console.WriteLine("You have " + question + " questions left");
             }
         }
     }
-
+}
